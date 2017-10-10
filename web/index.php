9721c6effb7fb165efc1337878ace56bd7706568
Larry Garfield <larry@platform.sh>
2017-10-10 12:49:26 -0500

    Use license key path as a file path, not dir path.

--- web/index.php
+++ web/index.php
@@ -7,10 +7,16 @@
 define('CRAFT_BASE_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
 
-define('CRAFT_LICENSE_KEY_PATH', CRAFT_BASE_PATH .'/license');
+define('CRAFT_LICENSE_KEY_PATH', CRAFT_BASE_PATH . 'license/license.key');
+
+print CRAFT_LICENSE_KEY_PATH . "<br />\n";
 
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH.'/autoload.php';
+
+
+$path = new \craft\services\Path();
+print_r("Derived path: %s<br />\n", $path->getLicenseKeyPath());
 
 // Load dotenv?
 if (file_exists(CRAFT_BASE_PATH.'/.env')) {
