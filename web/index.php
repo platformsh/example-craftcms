a11c9d7159e3cbe2b23c87ea44d6aca282c13401
Larry Garfield <larry@platform.sh>
2017-10-10 13:10:21 -0500

    The leading slash is needed afterall.

--- web/index.php
+++ web/index.php
@@ -7,16 +7,20 @@
 define('CRAFT_BASE_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
 
-define('CRAFT_LICENSE_KEY_PATH', CRAFT_BASE_PATH . 'license/license.key');
+define('CRAFT_LICENSE_KEY_PATH', CRAFT_BASE_PATH . '/license/license.key');
 
-print CRAFT_LICENSE_KEY_PATH . "<br />\n";
+define('CRAFT_SITE_URL', 'https://test-t6dnbai-myv5b63mxtdnw.us.platform.sh/');
+
+#printf("Raw constant for license key path: %s<br />\n", CRAFT_LICENSE_KEY_PATH);
+#printf("Raw constant for site Url: %s<br />\n", CRAFT_SITE_URL);
 
 // Load Composer's autoloader
 require_once CRAFT_VENDOR_PATH.'/autoload.php';
 
-
-$path = new \craft\services\Path();
-print_r("Derived path: %s<br />\n", $path->getLicenseKeyPath());
+#$path = new \craft\services\Path();
+#$config = new \craft\services\Config();
+#printf("License path: %s<br />\n", $path->getLicenseKeyPath());
+//print_r("Site URL: %s<br />\n", $config->getConfigSettings(\craft\services\Config::CATEGORY_GENERAL)->siteUrl);
 
 // Load dotenv?
 if (file_exists(CRAFT_BASE_PATH.'/.env')) {
